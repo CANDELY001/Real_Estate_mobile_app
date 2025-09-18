ce5fea24ab151567fc45508c40751e84c306305a
CANDELY001 <candely1charifa@gmail.com>
2025-09-18 02:35:04 +0100

    setup SDK, auth funtion and Auth integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-//import GlobalProvider from "@/lib/global-provider";
+import GlobalProvider from "@/lib/global-provider";
 import "./global.css";
 
 export default function RootLayout() {
@@ -26,5 +26,9 @@ export default function RootLayout() {
     return null;
   }
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
+  );
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
 
 const Auth = () => {
-  const handleLogin = () => {};
+  const { refetch, loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/" />;
+
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to login");
+    }
+  };
+
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView
@@ -22,7 +44,7 @@ const Auth = () => {
 
         <View className="px-10">
           <Text className="text-base text-center uppercase font-rubik text-black-200">
-            Welcome To ReState
+            Welcome To Real Scout
           </Text>
 
           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
