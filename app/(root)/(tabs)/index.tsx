0faf90fb612837196e3641e31efe160493493644
CANDELY001 <candely1charifa@gmail.com>
2025-09-20 18:17:15 +0100

    Explore ui

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -89,7 +89,7 @@ const Home = () => {
 
                 <View className="flex flex-col items-start ml-2 justify-center">
                   <Text className="text-xs font-rubik text-black-100">
-                    Good Morning
+                    Welcome back,
                   </Text>
                   <Text className="text-base font-rubik-medium text-black-300">
                     {user?.name}
